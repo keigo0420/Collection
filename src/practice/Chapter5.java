fb20c9b5f874035bc1c2b532b9987ae1405e98ea
KeigoIshibashi <105145869+keigo0420@users.noreply.github.com>
2022-07-11 10:26:23 +0900

    新規Javaプロジェクト作成

--- src/practice/Chapter5.java
+++ src/practice/Chapter5.java
@@ -1,11 +1,22 @@
 package practice;
 
-import java.util.LinkedHashSet;
+import java.time.*;
 import java.util.Set;
-
+import java.util.TreeSet;
 
 public class Chapter5 {
-	public static void main(String[]args) {
-	
-		
-	}}
+
+	public static void main(String[] args) {
+
+		Set<Task> list = new TreeSet<>();
+		list.add(new Task(LocalDate.of(2021, 10, 21), "牛乳を買う"));
+		list.add(new Task(LocalDate.of(2021, 9, 15), "○○社面接"));
+		list.add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う"));
+		list.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く"));
+		list.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く"));
+
+		for (Task d : list)
+			System.out.println(d);
+
+	}
+}
--- src/practice/Task.java
+++ src/practice/Task.java
@@ -1,31 +1,31 @@
 package practice;
 
-import java.util.*;
 import java.time.*;
 
-public class Task {
-	public static void main(String[]args) {
-		
-		
-		LocalDate l1 = LocalDate.of(2021,10,21);
-		LocalDate l2 = LocalDate.of(2021,9,15);
-		LocalDate l3 = LocalDate.of(2021,12,14);
-		LocalDate l4 = LocalDate.of(2021,8,10);
-		LocalDate l5 = LocalDate.of(2021,11,9);
-		
-		
-		
-		Set<String>tasks=new TreeSet<String>();
-		
-		tasks.add(l1+" :牛乳を買う。");
-		tasks.add(l2+":○○社面談");
-		tasks.add(l3+":手帳を買う。");
-		tasks.add(l4+":散髪に行く。");
-		tasks.add(l5+":スクールの課題を解く。");
-		
-		for(String t:tasks) { 
-		
-		System.out.println(t);
+public class Task implements Comparable<Task> {
+
+	private LocalDate date;
+	private String task;
+
+	public Task(LocalDate date, String task) {
+		this.date = date;
+		this.task = task;
 	}
 
-}}
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public String getTask() {
+		return task;
+	}
+
+	public int compareTo(Task t) {
+		return this.date.compareTo(t.date);
+	}
+
+	public String toString() {
+		return this.date + " " + this.task;
+	}
+
+}
